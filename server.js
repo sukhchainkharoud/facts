66dc1a8a0f3e44dbc854fef07613a49eb59a277c
sukhain <sukhchainkharoud7@gmail.com>
2017-05-31 16:13:51 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.get('/', (req, res, next) => {
+    
+    res.sendfile('./public/FACTS.html');
+
+});
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log("Server is up on port 3000");
+})
